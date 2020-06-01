eca4654a9acd41eff7b924c879d869a17b609cd7
Salumão Barbosa da Costa <salubcosta@gmail.com>
2020-06-01 01:52:54 -0300

    import footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './containers/Home';
 import About from './containers/About';
+import Footer from './components/Footer';
 
 const App = () => {
 
@@ -12,6 +13,7 @@ const App = () => {
             <Header />
             <Route path="/" exact component={Home} />
             <Route path="/about" exact component={About} />
+            <Footer />
         </Router>
     );
 }
